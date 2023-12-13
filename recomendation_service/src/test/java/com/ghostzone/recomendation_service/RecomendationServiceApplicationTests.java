183fbaef93cf35da47aff3e9755963afb76c4146
Jose Carrasco <jscarrasco18@outlook.com>
2023-12-13 11:21:58 -0400

    starting the streaming of music service

---
+++ recomendation_service/src/test/java/com/ghostzone/recomendation_service/RecomendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghostzone.recomendation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
